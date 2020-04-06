20feeda18b140aa962a38bafe0a8082f86a2a3bd
gitluin <kyluin@outlook.com>
2020-04-06 11:50:07 -0400

    updated examples for sbar-lemon and coding style

--- examples/.bash_profile
+++ examples/.bash_profile
@@ -4,6 +4,4 @@
 
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 
-export PATH=$PATH:"/ibin"
-
 [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
--- sarasock.c
+++ sarasock.c
@@ -16,7 +16,7 @@
 #include <unistd.h>
 
 /* this NEEDS to match with sara.c */
-#define INPUTSOCK			"/tmp/sara.sock"
+#define INPUTSOCK	"/tmp/sara.sock"
 
 int
 slen(const char* str){
